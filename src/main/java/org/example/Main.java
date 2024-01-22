b607e67aa0bf16fec7eb0e26c8e78bc4567a8273
anyquestions69 <danil_tyrenko@icloud.com>
2024-01-22 15:45:05 +0300

    Rework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,30 @@
 package org.example;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import org.antlr.v4.gui.TreeViewer;
-import org.antlr.v4.runtime.ANTLRFileStream;
-import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.TokenSource;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class Main {
     public static void main(String[] args) {
-        correctText("/Users/public_hysteria/Documents/antlrProj/src/main/resources/CorrectExample.txt");
-        incorrectText("/Users/public_hysteria/Documents/antlrProj/src/main/resources/ErrorExample.txt");
+        String absPath="C:\\Users\\Admin\\antlrproj\\src\\main\\resources\\";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название файла:");
+        String filename = scanner.nextLine();
+        analyzeText(absPath+filename);
+        System.out.println("Введите название файла:");
+        filename = scanner.nextLine();
+        analyzeText(absPath+filename);
     }
-    public static void correctText(String path){
-        boolean Success = false;
+    public static void analyzeText(String path){
+        System.out.println("\n\t\t~~ Анализ кода файла '"+path+"'~~");
         try{
             CharStream input = CharStreams.fromFileName(path);
             GramNewLexer lexer = new GramNewLexer(input);
@@ -38,47 +36,18 @@ public class Main {
             JPanel panel = new JPanel();
             TreeViewer viewer = new TreeViewer(Arrays.asList(
                     parser.getRuleNames()),tree);
-            viewer.setScale(0.7); // Scale a little
+            viewer.setScale(0.9);
             panel.add(viewer);
             frame.add(panel);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
             frame.pack();
             frame.setVisible(true);
-            if(parser.getTrimParseTree()) {
-                Success = true;
-            }
         }catch (java.lang.RuntimeException re){
-            System.out.println(re.getMessage());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if (Success){
-            System.out.println("Введенный код корректный");
-        }
-    }
-    public static void incorrectText(String path){
-        try{
-            CharStream input = CharStreams.fromFileName(path);
-            GramNewLexer lexer = new GramNewLexer(input);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            GramNewParser parser = new GramNewParser(tokens);
-            ParseTree tree = parser.r();
-            System.out.println(tree.toStringTree(parser));
-            JFrame frame = new JFrame("Antlr AST");
-            JPanel panel = new JPanel();
-            TreeViewer viewer = new TreeViewer(Arrays.asList(
-                    parser.getRuleNames()),tree);
-            viewer.setScale(0.7); // Scale a little
-            panel.add(viewer);
-            frame.add(panel);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setVisible(true);
 
-        }catch (java.lang.RuntimeException re){
             System.out.println(re.getMessage());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
